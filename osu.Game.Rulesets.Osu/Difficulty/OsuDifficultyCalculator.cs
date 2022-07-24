2e0da9511e50f16226086e04bcd86120c7221d29
02Naitsirk <iifrostii@outlook.com>
2022-07-24 19:02:40 -0400

    Nerf tap

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -24,7 +24,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
     {
         private const double aim_scaling = 20;
         private const double aim_exp = 0.829842642;
-        private const double tap_scaling = 0.125;
+        private const double tap_scaling = 0.10769;
         private const double tap_exp = 0.5;
 
         private double hitWindowGreat;
